2f4c52293b88d3a15e90683bf92cfbce6ed911ce
LubieKakao1212 <grzesio.go@gmail.com>
2024-02-18 11:53:58 +0100

    Ported forge version to fabric and moved to 1.19.4

---
+++ src/main/kotlin/com/lubiekakao1212/qulib/raycast/IntersectionPoints.kt
@@ -0,0 +1,13 @@
+package com.lubiekakao1212.qulib.raycast
+
+import org.joml.Vector3d
+
+data class IntersectionPoints(val distanceMin : Double, val distanceMax : Double, val pointNear : Vector3d, val pointFar : Vector3d) {
+    fun isValid(): Boolean {
+        return isInside() || distanceMin < distanceMax
+    }
+
+    fun isInside(): Boolean {
+        return (distanceMin < 0.0) xor (distanceMax < 0.0)
+    }
+}
